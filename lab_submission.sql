28d98b0478c666fac68fcf94c0b6b9b26629e855
Allan Omondi <aomondi@strathmore.edu>
2024-11-04 12:20:57 +0300

    Update lab_submission.sql

--- lab_submission.sql
+++ lab_submission.sql
@@ -1,7 +1,7 @@
--- (i) EXPLAIN output of any query of your choice that involves the “postalCode” of the “customers” relation in the WHERE clause and that does not use any index to retrieve its results.
+-- (i) A Procedure called PROC_LAB5
 
 
--- (ii) Code to create a B+ Tree index on the column “postalCode” in the table “customers”. 
+-- (ii) A Function called FUNC_LAB5
 
 
--- (iii) EXPLAIN output of the same query showing that it is using the created index to retrieve the results.
+-- (iii) A View called VIEW_LAB5
